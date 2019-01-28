b1d91964a1160b7cd839fa0b1058de7ab444895a
Brandon Lico <blico@uber.com>
2019-01-28 15:58:20 -0800

    Use upstream io_bazel gogoslick rule

--- language/go/generate.go
+++ language/go/generate.go
@@ -442,9 +442,9 @@ func (g *generator) generateProto(mode proto.Mode, target protoTarget, importPat
 	goProtoLibrary.SetAttr("proto", ":"+protoName)
 	g.setImportAttrs(goProtoLibrary, importPath)
 	if target.hasServices {
-		goProtoLibrary.SetAttr("compilers", []string{"//:gogo_slick", "//:go_yarpc"})
+		goProtoLibrary.SetAttr("compilers", []string{"@io_bazel_rules_go//proto:gogoslick_grpc", "//:go_yarpc"})
 	} else {
-		goProtoLibrary.SetAttr("compilers", []string{"//:gogo_slick"})
+		goProtoLibrary.SetAttr("compilers", []string{"@io_bazel_rules_go//proto:gogoslick_grpc"})
 	}
 	if g.shouldSetVisibility {
 		goProtoLibrary.SetAttr("visibility", visibility)
